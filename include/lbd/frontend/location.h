518a77553be768676ccc429f97969d8adc0bc771
ninthcircle <mainlakshayhoon@gmail.com>
2026-01-23 22:18:45 +0530

    Made fields inside location-structure private

--- include/lbd/frontend/location.h
+++ include/lbd/frontend/location.h
@@ -4,13 +4,13 @@
 
 namespace frontend {
     struct Location {
-        // TODO: Maybe make these members private.
-        size_t row;
-        size_t col;
-        std::string filepath;
-
         Location(size_t row, size_t col, std::string filepath);
 
         friend std::ostream &operator<<(std::ostream &stream, const Location &location);
+
+    private:
+        size_t row;
+        size_t col;
+        std::string filepath;
     };
 }
